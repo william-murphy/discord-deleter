05998c86ed15a68ca7404196e0cb2431c215de67
Will <wpm517@gmail.com>
2023-02-04 20:18:27 -0500

    discord bot is running

--- DiscordDeleter.py
+++ DiscordDeleter.py
@@ -1,7 +1,36 @@
+import discord
+import json
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
 class DiscordDeleter:
 
     def __init__(self):
-        print("Hello world")
+        self.config = self._getJson()
+        self.client = self._getClient()
+
+        self.client.run(self.config['token'])
+
+    def _getJson(self):
+        try:
+            with open('config.json') as f:
+                data = json.loads(f.read())
+            return data
+        except Exception as e:
+            print("config.json file not present in the current directory or has some issue")
+            quit()
+
+    def _getClient(self):
+        intents = discord.Intents(self.config['permissions'])
+
+        client = MyClient(intents=intents)
+        return client
 
 if __name__ == "__main__":
     DiscordDeleter()
--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # discord-deleter
 
-Discord bot that will delete messages containing any word from a given list of words
-
-## Build Instructions
-
-1. Install Python
-2. Run `python DiscordDeleter.py`
+Discord bot that will delete messages containing any word from a given list of words for any user.
