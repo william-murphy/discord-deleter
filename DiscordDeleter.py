b629a39e742530ba755251f6ede70dd42344aaa1
Will <wpm517@gmail.com>
2023-02-05 12:17:23 -0500

    finished, but it takes extremely long to run

--- DiscordDeleter.py
+++ DiscordDeleter.py
@@ -3,11 +3,39 @@ import json
 
 class MyClient(discord.Client):
 
+    command = "$remove"
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        # setup
+        splitMessage = message.content.split()
+        user = message.author
+        channel = message.channel
+
+        # guards
+        if not splitMessage[0] == "$remove":
+            return
+        del splitMessage[0]
+        if not len(splitMessage) > 0:
+            return
+
+        # success
+        await channel.send("Deleting your message...")
+        await message.delete()
+        await channel.send("Deleted message.")
+        await channel.send("Deleting messages containing the given words for user {}...".format(user))
+        try:
+            async for message in channel.history(limit=None):
+                print("running...")
+                for word in splitMessage:
+                    if word in message.content:
+                        await message.delete()
+        except Exception as e:
+            print(e)
+        else:
+            await channel.send("Finished deleting messages.")
 
 class DiscordDeleter:
 
@@ -22,15 +50,18 @@ class DiscordDeleter:
             with open('config.json') as f:
                 data = json.loads(f.read())
             return data
-        except Exception as e:
+        except:
             print("config.json file not present in the current directory or has some issue")
             quit()
 
     def _getClient(self):
-        intents = discord.Intents(self.config['permissions'])
+        intents = discord.Intents(value = self.config['permissions'])
+        intents.messages = True
+        intents.message_content = True
 
         client = MyClient(intents=intents)
         return client
+
 
 if __name__ == "__main__":
     DiscordDeleter()
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # discord-deleter
 
 Discord bot that will delete messages containing any word from a given list of words for any user.
+
+config.json file skeleton:
+
+
+`{
+    
+    "token": "",
+
+    "url": "",
+
+    "permissions": <some_int>
+}`
+
